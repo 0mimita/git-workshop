4eff0ed0a6ae018af0a6f9dac9456c6e022f8fdc
Mariam Tumah <tumahmariam@gmail.com>
2026-05-23 12:20:29 +0200

    visa restauranger

--- js/restaurants.js
+++ js/restaurants.js
@@ -6,13 +6,30 @@ async function getRestaurants() {
     try {
 
         const response = await fetch(
-            `https://smapi.lnu.se/api/?api_key=3bdTk4Bn
-            &controller=food&method=getfromlatlng&lat=${userLatitude}&lng=${userLongitude}&radius=5`
+            `https://smapi.lnu.se/api/?api_key=3bdTk4Bn&controller=food&method=getfromlatlng&lat=${userLatitude}&lng=${userLongitude}&radius=5`
         );
 
         const data = await response.json();
 
         console.log(data);
+
+        const container = document.getElementById("restaurants-container");
+
+        data.payload.forEach((restaurant) => {
+            const card = document.createElement("div");
+            card.classList.add("restaurant-card")
+            card.innerHTML = ` <h2>${restaurant.name}</h2>
+            <p>
+            ${restaurant.distance_in_km.toFixed(1)} km • 
+            ${restaurant.avg_lunch_pricing} kr
+            </p>
+            <p>
+             ${restaurant.rating} 
+            (${restaurant.num_reviews} omdömen)
+            </p>
+            `;
+            container.appendChild(card);
+        });
 
     } catch(error) {
 
