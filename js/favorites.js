49e66bf9acb7dddea4ed75c575d1e6095be176e5
Malak3333 <malak.mohamed5@icloud.com>
2026-05-25 15:55:25 +0200

    la till filter selection o spara till localStorage

--- js/favorites.js
+++ js/favorites.js
@@ -2,4 +2,43 @@ const favoriteButton = document.querySelector(".favorite-icon");
 
 favoriteButton.addEventListener("click", () => {
     favoriteButton.classList.toggle("active");
+
+    let favorites = localStorage.getItem("favorites");
+
+    if (favorites === null) {
+        favorites = [];
+    } else {
+        favorites = JSON.parse(favorites);
+    }
+
+    const currentRestaurant = JSON.parse(localStorage.getItem("selectedRestaurant"));
+
+    let exists = false;
+
+    for (let i = 0; i < favorites.length; i++) {
+        if (favorites[i].name === currentRestaurant.name) {
+            exists = true;
+
+        }
+    }
+
+    if (exists === true) {
+        let newFavorites = [];
+
+        for (let i = 0; i < favorites.length; i++) {
+            if (favorites[i].name !== currentRestaurant.name) {
+                newFavorites.push(favorites[i]);
+            }
+        }
+
+        favorites = newFavorites;
+    } else {
+        favorites.push(currentRestaurant);
+    }
+
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+
+    console.log(favorites);
+
+   
 });
--- js/filter.js
+++ js/filter.js
@@ -6,5 +6,44 @@ buttons.forEach((button) => {
         
         button.classList.toggle("selected");
 
+        let selectedFilters = localStorage.getItem("filters");
+
+        if (selectedFilters === null) {
+            selectedFilters = [];
+        } else {
+            selectedFilters = JSON.parse(selectedFilters);
+        }
+
+        const value = button.textContent;
+
+        let alreadySelected = false;
+
+        for (let i = 0; i < selectedFilters.length; i++) {
+            if (selectedFilters[i] === value) {
+                alreadySelected = true;
+            }
+        }
+
+        if (alreadySelected === true) {
+            let newArray = [];
+
+            for (let i = 0; i < selectedFilters.length; i++) {
+                if (selectedFilters[i] !== value) {
+                    newArray.push(selectedFilters[i]);
+                }
+        }
+        selectedFilters = newArray;
+    } else {
+        selectedFilters.push(value);
+    }
+
+    localStorage.setItem(
+        "filters",
+        JSON.stringify(selectedFilters)
+    );
+
+    console.log(selectedFilters);
+
     });
+    
 });
