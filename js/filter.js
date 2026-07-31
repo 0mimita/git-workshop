71b0f71d15e394cc6606630dedb51f96b0513b93
Mariam Tumah <tumahmariam@gmail.com>
2026-07-31 22:37:35 +0200

    revert

--- js/filter.js
+++ js/filter.js
@@ -94,7 +94,12 @@ if (clearBtn) {
 const tooltip = document.querySelector(".tooltip");
 
 if (tooltip) {
-    tooltip.addEventListener("click", () => {
+    tooltip.addEventListener("click", (event) => {
+        event.stopPropagation();
         tooltip.classList.toggle("show");
+    });
+
+    document.addEventListener("click", () => {
+        tooltip.classList.remove("show");
     });
 }
